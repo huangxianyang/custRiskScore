0a9ee0505e0983d0a21b9512302434dc59c35d99
黄先阳 <huangxianyang@qinjia001.com>
2021-11-10 22:39:49 +0800

    port

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     uvicorn.run(
         app='main:app',
         host='0.0.0.0',
-        port=8004,
+        port=8005,
         debug=True,
         reload=False
     )
